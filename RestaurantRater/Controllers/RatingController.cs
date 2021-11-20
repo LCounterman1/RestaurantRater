ad2c827b0d4d83d4864c72f11d5a6fdc815e1416
Leslie Counterman <LCounterman1@gmail.com>
2021-11-20 14:59:45 -0500

    first commit

---
+++ RestaurantRater/Controllers/RatingController.cs
@@ -0,0 +1,63 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RatingController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext
+
+        // Creat new ratings
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateRating([FromBody] Rating model)
+        {
+            // check if model is null
+            if (model is null)
+                return BadRequest("Your request body cannot by empty.");
+
+            // check if model state is invalid 
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            // find the Restaurant by model.RestaurantId and see that it exists
+            var restaurantEntity = await _context.Restaurants.FindAsync(model.RestaurantId);
+            if (restaurantEntity is null)
+                return BadRequest($"The target restaurant with the ID of {model.RestaurantId}" +
+                    $" does not exist.");
+
+
+            // Create the Rating
+
+            // Add to the Rating table
+            //_context.Ratings.Add(model);
+
+            // Add to the Rataurant Entity
+            restaurantEntity.Ratings.Add(model);
+            if (await _context.SaveChangesAsync() == 1)
+                return Ok($"You rated restaurant {restaurantEntity.Name} successfully!");
+
+            return InternalServerError();
+
+
+        }
+
+
+
+
+        //Get a Rating by its id
+
+        // Get All Ratings
+
+        // Get All Ratings for specific restaurant by restaurant id
+
+        // Update a Rating
+
+        // Delete a Rating
+    }
+}
---
+++ RestaurantRater/Controllers/RestaurantController.cs
@@ -0,0 +1,118 @@
+﻿using RestaurantRater.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+
+        // POST (create)
+        // api/Restaurant
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateRestaurant([FromBody] Restaurant model)
+        {
+            if (model is null)
+            {
+                return BadRequest("Your request body cannot be empty");
+            }
+
+            if (ModelState.IsValid)
+            {
+                // Store model in the db
+                _context.Restaurants.Add(model);
+                int changeCount = await _context.SaveChangesAsync();
+
+                return Ok("Your restaurant was created.");
+            }
+
+            // The model is not valid, reject it
+            return BadRequest(ModelState);
+        }
+
+        // GET ALL
+        // api/Restaurant
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+            return Ok(restaurants);
+        }
+
+
+        // GET BY ID
+        // api/Restaurant/id
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById([FromUri] int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant != null)
+            {
+                return Ok(restaurant);
+            }
+
+            return NotFound();
+           
+        }
+
+        // Put (update)
+        // api/Restaurant/(id)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri] int id, [FromBody] Restaurant updatedRestaurant)
+        {
+            // check the ids if they match
+            if (id != updatedRestaurant?.Id)
+            {
+                return BadRequest("Ids do not match");
+            }
+
+            // check the model state
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+
+            // find the restaurant in the database
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            // if the restaurant does not exist then do something
+            if (restaurant is null)
+                return NotFound();
+
+            // update the properties
+            restaurant.Name = updatedRestaurant.Name;
+            restaurant.Address = updatedRestaurant.Address;
+
+            // save the changes
+            await _context.SaveChangesAsync();
+
+            return Ok("The restaurant was updated!");
+        }
+
+        // Delete 
+        // api/Restaurant/(id)
+        [HttpDelete]
+        public async Task<IHttpActionResult> DeleteRestaurant([FromUri] int id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+            if (restaurant is null)
+                return NotFound();
+
+            _context.Restaurants.Remove(restaurant);
+
+            if (await _context.SaveChangesAsync() == 1)
+            {
+                return Ok("The restaurant was deleted.");
+            }
+            return InternalServerError();
+        }
+    }
+}
